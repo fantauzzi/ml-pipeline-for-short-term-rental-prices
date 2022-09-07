251dc573fd6e277bf54f5923edd8d7eab04bf3e7
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2022-09-07 17:04:18 +0200

    Added the eda step

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ def go(config: DictConfig):
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
                 "main",
+                version="main",
                 parameters={
                     "sample": config["etl"]["sample"],
                     "artifact_name": "sample.csv",
@@ -52,6 +53,7 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
+
             pass
 
         if "data_check" in active_steps:
