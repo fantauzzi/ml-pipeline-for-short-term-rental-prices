d99beeb0486786e6b4163a40aa679b7b985577d5
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2022-09-07 20:13:04 +0200

    Implemented the basic cleaning

--- main.py
+++ main.py
@@ -5,75 +5,87 @@ import tempfile
 import os
 import wandb
 import hydra
+import logging
 from omegaconf import DictConfig
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(message)s')
+logger = logging.getLogger()
+
 _steps = [
-    "download",
-    "basic_cleaning",
-    "data_check",
-    "data_split",
-    "train_random_forest",
+    'download',
+    'basic_cleaning',
+    'data_check',
+    'data_split',
+    'train_random_forest',
     # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
+    # You first need to promote a model export to 'prod' before you can run this,
     # then you need to run this step explicitly
-#    "test_regression_model"
+    #    'test_regression_model'
 ]
 
 
 # This automatically reads in the configuration
 @hydra.main(config_name='config')
 def go(config: DictConfig):
-
-    # Setup the wandb experiment. All runs will be grouped under this name
-    os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
-    os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
+    # Set up the wandb experiment. All runs will be grouped under this name
+    os.environ['WANDB_PROJECT'] = config['main']['project_name']
+    os.environ['WANDB_RUN_GROUP'] = config['main']['experiment_name']
 
     # Steps to execute
     steps_par = config['main']['steps']
-    active_steps = steps_par.split(",") if steps_par != "all" else _steps
+    active_steps = steps_par.split(',') if steps_par != 'all' else _steps
+    logging.info(f'Executing step(s): {steps_par}')
 
     # Move to a temporary directory
     with tempfile.TemporaryDirectory() as tmp_dir:
 
-        if "download" in active_steps:
-            # Download file and load in W&B
+        if 'download' in active_steps:
+            # Upload file into W&B
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
-                "main",
-                version="main",
+                'main',
+                version='main',
                 parameters={
-                    "sample": config["etl"]["sample"],
-                    "artifact_name": "sample.csv",
-                    "artifact_type": "raw_data",
-                    "artifact_description": "Raw file as downloaded"
+                    'sample': config['etl']['sample'],
+                    'artifact_name': 'sample.csv',
+                    'artifact_type': 'raw_data',
+                    'artifact_description': 'Raw file as downloaded'
                 },
             )
 
-        if "basic_cleaning" in active_steps:
+        if 'basic_cleaning' in active_steps:
+            # Hydra doesn't run in the root of the GitHub project, therefore getting the right directory with
+            # get_original_cwd()
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), 'src', 'basic_cleaning'),
+                'main',
+                parameters={
+                    'input_artifact': 'sample.csv:latest',
+                    'output_artifact': 'clean_sample.csv',
+                    'output_type': 'clean_sample',
+                    'output_description': 'Data with outliers and null values removed',
+                    'min_price': config['etl']['min_price'],
+                    'max_price': config['etl']['max_price']
+                },
+            )
+
+        if 'data_check' in active_steps:
             ##################
             # Implement here #
             ##################
-
             pass
 
-        if "data_check" in active_steps:
+        if 'data_split' in active_steps:
             ##################
             # Implement here #
             ##################
             pass
 
-        if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
-
-        if "train_random_forest" in active_steps:
-
+        if 'train_random_forest' in active_steps:
             # NOTE: we need to serialize the random forest configuration into JSON
-            rf_config = os.path.abspath("rf_config.json")
-            with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
+            rf_config = os.path.abspath('rf_config.json')
+            with open(rf_config, 'w+') as fp:
+                json.dump(dict(config['modeling']['random_forest'].items()), fp)  # DO NOT TOUCH
 
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
@@ -84,8 +96,7 @@ def go(config: DictConfig):
 
             pass
 
-        if "test_regression_model" in active_steps:
-
+        if 'test_regression_model' in active_steps:
             ##################
             # Implement here #
             ##################
---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+"""
+The MLFlow project uses Weights and Biases
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(message)s')
+logger = logging.getLogger()
+
+
+def go(args):
+    output_file_name = 'clean_sample.csv'
+    run = wandb.init(job_type='basic_cleaning')
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    ######################
+    # YOUR CODE HERE     #
+    ######################
+
+    artifact = run.use_artifact(args.input_artifact)
+    file_path = artifact.file()
+    min_price = args.min_price
+    max_price = args.max_price
+    df = pd.read_csv(file_path)
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    df.to_csv(output_file_name, index=False)
+    logging.info(f'Saved cleaned data into {output_file_name}')
+
+    artifact = wandb.Artifact(args.output_artifact,
+                              type=args.output_type,
+                              description=args.output_description)
+    artifact.add_file((output_file_name))
+    run.log_artifact(artifact)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Essential data cleaning')
+
+    parser.add_argument(
+        '--input_artifact',
+        type=str,
+        help='The raw data to be cleaned',
+        required=True
+    )
+
+    parser.add_argument(
+        '--output_artifact',
+        type=str,
+        help='The cleaned data (the output artifact).',
+        required=True
+    )
+
+    parser.add_argument(
+        '--output_type',
+        type=str,
+        help='The type for the output artifact.',
+        required=True
+    )
+
+    parser.add_argument(
+        '--output_description',
+        type=str,
+        help='A description for the output artifact.',
+        required=True
+    )
+
+    parser.add_argument(
+        '--min_price',
+        type=float,
+        help='The minimum price to consider.',
+        required=True
+    )
+
+    parser.add_argument(
+        '--max_price',
+        type=float,
+        help='The maximum price to consider.',
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    go(args)
